040b3954c9211ee80e841b0dfecbe8928394eadb
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2018-09-23 19:50:39 +0200

    Removed react logo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Marvel Comics Directory</h1>
         </header>
         <p className="App-intro">
